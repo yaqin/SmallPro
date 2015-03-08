656c955b305b46745c6cd920d65ab4ed7a95eb9b
yaqin <yaqin276@gmail.com>
2015-03-08 12:42:48 -0700

    add white space after annotation

--- src/main/python/modifyXML.py
+++ src/main/python/modifyXML.py
@@ -9,6 +9,7 @@ class Extract(object):
         self.f_loc = sys.argv[1]
         self.output = sys.argv[1]+"/extract_ouput/"
         self.f = os.listdir(self.f_loc)
+        print self.f
 
     def load_data(self):
         for x in self.f:
@@ -26,7 +27,7 @@ class Extract(object):
                 newtext = text[:dic_start_tag[0][0]] + dic_start_tag[0][1] + "_#"
                 for i in range(1, len(dic_start_tag)):
                     newtext = newtext + text[dic_start_tag[i - 1][0] + 5:dic_start_tag[i][0]] + dic_start_tag[i][
-                        1] + "_#"
+                        1] + "_# "
                 newtext += text[dic_start_tag[len(dic_start_tag) - 1][0] + 5:]
                 self.root[0].text = newtext
                 output_name_temp = self.output + "temp" + x
