c648fb9022291a4e8f3544081729e1624c65d94a
yaqin <yaqin276@gmail.com>
2015-03-09 22:02:50 -0700

    test genDriver

--- src/main/python/genDriver.py
+++ src/main/python/genDriver.py
@@ -350,11 +350,11 @@ if __name__ == "__main__":
     task_dir = data["task_dir"]
     #annotation_dict = readCSV(annotation_file)
     #task_dict = readTask(task_dir)
-    cmodel = gensim.models.word2vec.Word2Vec.load_word2vec_format(gigaword_vector,binary=True)
-    data["cmodel"] = cmodel
+#     cmodel = gensim.models.word2vec.Word2Vec.load_word2vec_format(gigaword_vector,binary=True)
+#     data["cmodel"] = cmodel
     #readAnnotatedData(annotation_dict,task_dict,**data)
     #readSMS(**data)
-    readStagesTrain(**data)
+#     readStagesTrain(**data)
     #writeCSV(annotation_file+".clean",annotation_dict)
 
     #cmodel = gensim.models.word2vec.Word2Vec.load_word2vec_format(gigaword_vector,binary=True)
